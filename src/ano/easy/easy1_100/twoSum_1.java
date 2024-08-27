a795dab24e9c2306e17fb57675b6515b1de409ca
cwx <cwx02040673@alibaba-inc.com>
2024-08-27 17:53:20 +0800

    bug man

--- src/ano/easy/easy1_100/twoSum_1.java
+++ src/ano/easy/easy1_100/twoSum_1.java
@@ -1,5 +1,10 @@
 package ano.easy.easy1_100;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class twoSum_1 {
 
     class Solution {
@@ -20,17 +25,102 @@ public class twoSum_1 {
             //但是绝对要从健壮性出发
 
 
-            // 个人思路
+            //? 个人思路1 : 大小灵活判断(偏贪心)
             //! 由于只需要输出一个可能的解, 并且元素是不能重复使用的
             // 直接取一个, 定死了之后去扫描另一个, 这就是一般想法了. n^2复杂度. for循两层.
             // 优化: 仍然是全扫描, 但是先排序一次, 使用降序排序(系统自带快排). 这样就得到了一个有序的序列了
             // 因为是加法, 并且是越加越多, 直接一口吃最大的一份, 选[0]号元素; 然后判断是不是超过了target.
-            // 根据比较结果进行 聪明 判断:
-            // if > target: 用一个指针从最小端往回遍历, 直到找到; 如果小于了就报错
-            // if < target: 同样用一个指针
+            // 根据[0]号元素的比较结果进行 聪明 判断:
+            // if > target: 用一个指针从最小端往回遍历, 直到找到; 如果小于了就报错(不存在)
+            // if < target: 用一个指针指向余下元素的中间位置. 取一位判断
+            // 根据结果进行判断, 重复上述步骤
 
-            return null;
+
+            //* 定义变量
+
+            //快速排序nums: 并要求记录其原先位置和现在的对照关系
+            Map<Integer, Integer> map = new HashMap<>();
+            int[] num_new= Arrays.copyOf(nums, nums.length);
+            Arrays.sort(num_new);
+
+            //创建Map 关联原先位置和现在位置
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], num_new[i]);
+            }
+
+            //取最大元素[0]
+            int p;
+
+//            if (nums[0] > target) {//判断是否超过target
+//                //p从最小端往回遍历, 直到找到; 如果小于了就报错
+//                p = nums.length - 1;
+//                while (p > 0) {
+//                    if (nums[0] + nums[p] == target) {
+//                        return new int[]{0, p};
+//                    }
+//                    p--;
+//                }
+//            }
+//            else{
+//                //p指向余下元素的中间位置
+//                p = nums.length / 2;
+//                while (p > 0 && p < nums.length) {
+//                    if (nums[0] + nums[p] > target) {
+//                        p++;
+//                    } else if (nums[0] + nums[p] < target) {
+//                        p--;
+//                    } else {
+//                        return new int[]{0, p};
+//                    }
+//                }
+//            }
+
+            //fix:
+            if (num_new[0] > target) {//判断是否超过target
+                //p从最小端往回遍历, 直到找到; 如果小于了就报错
+                p = nums.length - 1;
+                while (p > 0) {
+                    if (num_new[0] + num_new[p] == target) {
+                        return new int[]{map.get(num_new[0]), map.get(num_new[p])};
+                    }
+                    p--;
+                }
+            }
+            else{
+                //p指向余下元素的中间位置
+                p = nums.length / 2;
+                while (p > 0 && p < nums.length) {
+                    if (num_new[0] + num_new[p] > target) {
+                        p++;
+                    } else if (num_new[0] + num_new[p] < target) {
+                        p--;
+                    } else {
+                        return new int[]{map.get(num_new[0]), map.get(num_new[p])};
+                    }
+                }
+            }
+
+
+            //? 个人思路2 : 暴力for扫描(对照组)
+
+
+            //? 个人思路3 : 1的修改型(都使用单指针聪明判断)
+
+            //? 个人思路4 : 标准双指针
+
+
+            return new int[]{0, 0};
         }
+    }
+
+
+    public static void main(String[] args) {
+        // 测试
+        Solution solution = new twoSum_1().new Solution();
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] res = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(res));
     }
 
 }
