0f56c051915f0c81922637f8f5a566fad33ffd0a
cwx <cwx02040673@alibaba-inc.com>
2024-08-30 15:18:05 +0800

    四相生八卦

---
+++ src/ano/easy/easy1_100/lengthOfLongestSubstring_3.java
@@ -0,0 +1,12 @@
+package ano.easy.easy1_100;
+
+public class lengthOfLongestSubstring_3 {
+
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/ano/easy/easy1_100/twoSum_1.java
+++ src/ano/easy/easy1_100/twoSum_1.java
@@ -5,34 +5,30 @@ import java.util.*;
 
 public class twoSum_1 {
 
-    class Solution {
-
-
-
-        /**
-         * 两数之和
-         */
-        public int[] twoSum(int[] nums, int target) {
+    /**
+     * 两数之和
+     */
+    public int[] twoSum(int[] nums, int target) {
             /*给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
             你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素*/
-            //? 输入： nums = [3,2,4], target = 6
-            //? 输出：[1,2]
+        //? 输入： nums = [3,2,4], target = 6
+        //? 输出：[1,2]
 
-            if (Objects.isNull(nums) || nums.length < 2) {
-                return new int[]{0, 0};
-            }
-            if (nums.length == 2) {
-                return new int[]{0, 1};
-            }
+        if (Objects.isNull(nums) || nums.length < 2) {
+            return new int[]{0, 0};
+        }
+        if (nums.length == 2) {
+            return new int[]{0, 1};
+        }
 
-            //----
+        //----
 
-            //坑点解析: 只是整数, 可能有负数, 导致最后不一定是逐渐往上加, 并且最大的元素可能比target还要大. 如果忽略这个性能估计会进一步提升
-            //但是绝对要从健壮性出发
+        //坑点解析: 只是整数, 可能有负数, 导致最后不一定是逐渐往上加, 并且最大的元素可能比target还要大. 如果忽略这个性能估计会进一步提升
+        //但是绝对要从健壮性出发
 
 
-            //? 暴力for扫描(对照组)
-            //double for
+        //? 暴力for扫描(对照组)
+        //double for
             /*for (int i = 0; i < nums.length; i++) {
                 for (int j = i + 1; j < nums.length; j++) {
                     if (nums[i] + nums[j] == target) {
@@ -41,52 +37,47 @@ public class twoSum_1 {
                 }
             }*/
 
-            //? 标准双指针
-            // 类似排列组合思路
-            int first = 0, second = 1;
-            while (first < nums.length) {
-                while(second < nums.length) {
-                    if (nums[first] + nums[second] == target) {
-                        return new int[]{first, second};
-                    }
-                    second += 1;
+        //? 标准双指针
+        // 类似排列组合思路
+        int first = 0, second = 1;
+        while (first < nums.length) {
+            while (second < nums.length) {
+                if (nums[first] + nums[second] == target) {
+                    return new int[]{first, second};
                 }
-                first += 1;
-                second = first + 1;
+                second += 1;
             }
-
-
-            return new int[]{0, 0};
-
-
-
+            first += 1;
+            second = first + 1;
         }
+
+
+        return new int[]{0, 0};
+
 
     }
 
 
     public static void main(String[] args) {
 
-        Solution solution = new twoSum_1().new Solution();
-
         int[] nums, res;
         int target;
 
         nums = new int[]{3, 2, 4};
         target = 6;
-        res = solution.twoSum(nums, target);
+        res = new twoSum_1().twoSum(nums, target);
         System.out.println(Arrays.toString(res));
         //? 输出：[1,2]
 
         nums = new int[]{2, 7, 11, 15};
         target = 13;
-        res = solution.twoSum(nums, target);
+        res = new twoSum_1().twoSum(nums, target);
         System.out.println(Arrays.toString(res));
         //? 输出：[0,2]
 
         nums = new int[]{1, 7, 5, 3, 2, 9, 4};
         target = 16;
-        res = solution.twoSum(nums, target);
+        res = new twoSum_1().twoSum(nums, target);
         System.out.println(Arrays.toString(res));
         //? 输出：[1,5]
 
