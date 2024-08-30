8f11b6e28dfd6e7155ec7c713fb8e41cc1544865
cwx <cwx02040673@alibaba-inc.com>
2024-08-30 16:38:13 +0800

    队列 双O(N) 90% 击破

--- src/ano/easy/easy1_100/lengthOfLongestSubstring_3.java
+++ src/ano/easy/easy1_100/lengthOfLongestSubstring_3.java
@@ -1,6 +1,7 @@
 package ano.easy.easy1_100;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class lengthOfLongestSubstring_3 {
 
@@ -12,32 +13,48 @@ public class lengthOfLongestSubstring_3 {
     public int lengthOfLongestSubstring(String s) {
         //个人思路:
         //!1. 标解滑动窗口, 过一下
-        //!2. 栈实现, 遇到相同栈内元素则刷新并记录, 取最大值
+        //!2. 队列实现, 遇到相同队列内元素则刷新并记录, 取最大值
 
-        //2.栈实现:
-        Stack<Character> stack = new Stack<>();
-        int maxLength = 0;
-        int nowLength = 0;
-        char[] sc = s.toCharArray();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (stack.search(sc[i]) == -1) { //找不到, 进来
-                nowLength += 1;
-                stack.push(sc[i]);
-            }
-            //找到了, 比较最大值
-            if(nowLength>maxLength) maxLength=nowLength;
-            nowLength = 1;
-            stack.clear();
-            stack.push(sc[i]);
+        //短路
+
+        if (s.length() == 1) {
+            return 1;
+        }
+        if (s.length() == 2 && s.toCharArray()[0] == s.toCharArray()[1]) {
+            return 1;
+        }
+        if (s.length() == 2 && s.toCharArray()[0] != s.toCharArray()[1]) {
+            return 2;
         }
 
+        //2.队列实现:
+        Queue<Character> queue = new ArrayDeque<>();
+        int maxLength = 0;
+        char[] sc = s.toCharArray();
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            if (queue.contains(sc[i])) {  //出队直到当前队尾的和sc[i]相同
+                do {
+                    c = queue.poll();
+                } while (c != sc[i]);
+            }
+            queue.add(sc[i]);
+            if (queue.size() > maxLength) maxLength = queue.size();
+        }
 
         return maxLength;
     }
 
     public static void main(String[] args) {
-        String s = "abcabcbb";
+        String s;
+        s = "abcabcbb";
+        System.out.println(new lengthOfLongestSubstring_3().lengthOfLongestSubstring(s));
+
+        s = "aab";
+        System.out.println(new lengthOfLongestSubstring_3().lengthOfLongestSubstring(s));
+
+        s = "dvdf";
         System.out.println(new lengthOfLongestSubstring_3().lengthOfLongestSubstring(s));
     }
 }
