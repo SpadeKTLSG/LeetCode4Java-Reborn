057f8caec2112ec5d2b2ea823d325c4cf862f38f
cwx <cwx02040673@alibaba-inc.com>
2024-09-03 10:07:50 +0800

    往日重现

---
+++ src/ano/easy/easy1_100/longestCommonPrefix_14.java
@@ -0,0 +1,14 @@
+package ano.easy.easy1_100;
+
+public class longestCommonPrefix_14 {
+
+    //查找字符串数组中的最长公共前缀。
+    //如果不存在公共前缀，返回空字符串 ""
+
+    //思路
+    //! 一 字典. 将各个字符串 -> (char : sit), 然后查找匹配
+    //! 二 多指针
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+}
