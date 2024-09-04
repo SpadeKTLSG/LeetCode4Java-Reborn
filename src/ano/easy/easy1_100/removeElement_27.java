c8e20805af45dbb43f9e2fbc9a23709648a890c1
cwx <cwx02040673@alibaba-inc.com>
2024-09-04 16:27:11 +0800

    快慢针2

--- src/ano/easy/easy1_100/removeElement_27.java
+++ src/ano/easy/easy1_100/removeElement_27.java
@@ -1,4 +1,15 @@
 package ano.easy.easy1_100;
 
 public class removeElement_27 {
+
+    //移除元素
+
+    //给你一个数组 nums 和一个值 val，原地 移除所有数值等于 val 的元素
+    //元素的顺序可能发生改变。然后返回 nums 中 与 val 不同的元素的数量
+
+    //输入：nums = [3,2,2,3], val = 3
+    //输出：2, nums = [2,2,_,_]
+
+    //思路
+    //! 一 和上面的一样的快慢针, 略过
 }
